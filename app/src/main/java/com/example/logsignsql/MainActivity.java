9c3d1e1e690ba5f5685e1607fddd397f573b3671
martin-delhugo <martindelhugo@mi.unc.edu.ar>
2023-09-24 16:37:28 -0300

    Se agrega funcionalidad de registro de analisis

--- app/src/main/java/com/example/logsignsql/MainActivity.java
+++ app/src/main/java/com/example/logsignsql/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.logsignsql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void irRegistroAnalisis(View v){
+        Intent siguiente = new Intent(this, RegistoAnalisis.class);
+        startActivity(siguiente);
+    }
+
+
 }
